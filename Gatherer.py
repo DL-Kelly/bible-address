0c03b62d58ac3264d8974a48fc101953350faddd
DL-Kelly <kellyderrin@gmail.com>
2023-10-02 18:46:44 -0500

    bible-address

--- Gatherer.py
+++
@@ -1,7 +0,0 @@
-"""from json import loads, dumps
-from re import match
-with open("NKJV.json") as nkjv:
-    json_dict = loads(u"" + nkjv.read())
-    for book in json_dict["books"]:
-        with open("Books/%s.json" % book["name"], "w") as sep_book:
-            sep_book.write(dumps(book))"""
